0bd23fbf46c91645a0f35dd294bf9d778d4ad0b0
Mrdjan Stajic <mrdjan@videobolt.com>
2018-09-02 11:16:53 +0200

    keyz

--- const.js
+++ const.js
@@ -1,6 +1,10 @@
 import { CLOUDINARY_INFO, SOME_API_SECRET } from "./secrets";
 
 export const POST_TYPES = ['Food', 'Lifestyle', 'Tech']
-export const cloud_key = CLOUDINARY_INFO;
+export const cloud_key = {
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+};
 
-export const api_secret = SOME_API_SECRET;
+export const api_secret = process.env.API_SECRET;
